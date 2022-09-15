9903271e1a52413fb226671122983014e5745b3e
Joao-Neto-DH <joaolima882018@gmail.com>
2022-09-15 13:25:01 +0100

    Adicionando o Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
+import BannerText from "./components/BannerText";
+import Logo from "./components/Logo";
 import "./styles/main.css";
-// import { useState } from 'react'
-
-interface Props{
-  title?: string;
-}
-
-function Button(props: Props) {
-  return <button>{props.title}</button>
-}
 
 function App() {
   return (
-    <div className="w-8 h-8 bg-violet-600 md:bg-red-700">
-      <h1>Hello World</h1>
-      <br/>
-      <Button title="Send"></Button>
-      <Button></Button>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center">
+      <Logo/>
+      <BannerText/>
     </div>
   )
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+import logo_nlw from "../assets/logo-nlw-esports.svg";
+
+function Logo() {
+    return <img src={logo_nlw} alt="NLW Logo" className="my-20"/>
+}
+
+export default Logo;
