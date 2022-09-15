67ebfeb548001b2159ba24093c714f8f42e2da33
Joao-Neto-DH <joaolima882018@gmail.com>
2022-09-15 13:49:12 +0100

    Adicionando BannerText component

---
+++ src/components/BannerText.tsx
@@ -0,0 +1,13 @@
+function BannerText() {
+    // const backgroundHighlight = {
+    //     // "-webkit-background-clip": "text",
+    //     // "-webkit-text-fill-color": "transparent",
+    //     "background-clip": "text",
+    //     "text-fill-color": "transparent"
+    // };
+    return ( <h1 className="text-6xl text-white font-bold">
+                Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.
+             </h1> );
+}
+
+export default BannerText;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
+        "galaxy": "url('/background-galaxy.png')",
+        "nlw-gradient": "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)" 
       }
     },
   },
